cf30447dfb28b2d9a75bf5207c590bbb27ca751f
sukhendra <mpct5jan1998@gmail.com>
2023-03-10 22:52:26 +0530

    js added

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 let btns = document.querySelectorAll(".btnLink");
+let closeBtn = document.querySelectorAll("span");
 
 const openTextBox = (eve, textBoxVal) => {
-  // let i, tabContent, tabLinks;
   let valueStr = textBoxVal.toString();
-  // console.log(valueStr);
+
   let boxes = document.getElementsByClassName("box");
 
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].style.display = "none";
   }
 
-  console.log("Hello");
   document.getElementById(valueStr).style.display = "block";
+//   eve.currentTarget.classList.add('active:bg-gray-600');
 };
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     let value = e.currentTarget.innerText;
-    // console.log(e.currentTarget);
-    // console.log(value);
     openTextBox(e, value);
   });
 });
+
+closeBtn.forEach((span) => {
+  span.addEventListener("click", (e) => {
+    e.currentTarget.parentElement.style.display = "none";
+  });
+});
+document.getElementById("defaultOpen").click();
